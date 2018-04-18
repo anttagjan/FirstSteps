211984fc1ce78c7bf2774cc3c855f000e524557d
anttagjan <38495085+anttagjan@users.noreply.github.com>
2018-04-18 15:24:20 +0200

    Change accents

--- src/no_valid_cells.m
+++ src/no_valid_cells.m
@@ -1,15 +1,15 @@
 %Cargamos el archivo
 img=imread('Image_1_Diagram_16.png');
-%Binarización y watershed
+%Binarizacion y watershed
 img_bw=im2bw(img);
 img_w=watershed(-img_bw);
-%Etiquetar las células
+%Etiquetar las celulas
 [img_label,NUM]=bwlabeln(img_w);
-%Crear un cell array con las distintas células en imagenes diferentes
+%Crear un cell array con las distintas celulas en imagenes diferentes
 for i=1:NUM
   cells{i,1}= (img_w==i);
 end 
-%Establecer células válidas y no válidas
+%Establecer celulas validas y no validas
 for i=1:NUM
     study_cell=cell2mat(cells(i,:));
     if sum(study_cell(1,:)) >=1 || sum(study_cell(:,1))>=1 || sum(study_cell(1024,:))>=1 || sum(study_cell(:,512))>=1
@@ -20,7 +20,3 @@ for i=1:NUM
 end
 
 
-        
-
-% Para el area puedo usar el comando usado en la practica 2 de la asignatura de imagen:
-% fprintf('Area mascara neurona 1: %f\n',sinhalo_1)
