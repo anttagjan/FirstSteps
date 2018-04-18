f16dee5cf8df56c1b80bb30851ca8ea16772d605
anttagjan <38495085+anttagjan@users.noreply.github.com>
2018-04-18 18:05:11 +0200

    calculate neighbours
    Calculate number of neighbours of each cell and describe neighbours' labels

--- src/calculate_neighbours.m
+++ src/calculate_neighbours.m
@@ -1,10 +1,25 @@
-%Abrir cell array con celulas validas
-load valid_cells.mat
+%Abrir archivo requerido
 img=imread('Image_1_Diagram_16.png');
+%Binarizar la imagen y etiquetar las celulas
 img_bw=im2bw(img);
-img_w=watershed(-img_bw);
-[img_label,NUM]=bwlabeln(img_w);
+img_label=bwlabel(img_bw);
+%Establecer las celulas no validas
+no_val=no_valid_cells2(img_label);
 
-se = strel('disk', 2);
-img_dilate = imdilate(img_label, se);
-sum(Neurona_halo_1(:));
+%Establecer kernel de dilatacion
+se = strel('square', 8);
+
+neighbours=0;
+w=zeros(1,60);
+cells_neighbours=[];
+%Dilatacion de las celulas para comprobar cuales comparten pixeles y
+%establecer el numero de vecinos y las etiquetas de los vecinos
+for i=1:60
+    neighbours= unique(img_label(imdilate(img_label==i,se) == 1));
+    neighbours(neighbours== 0)=[];
+    neighbours(neighbours ==i)=[];
+    neighbours_labels{i,1}=neighbours;
+    total_neighbours=length(neighbours);
+    cells_neighbours{i,1}= total_neighbours;
+
+end 
