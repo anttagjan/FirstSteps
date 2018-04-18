d1bcaeebae58d553d31f161582c715e0d2927542
anttagjan <38495085+anttagjan@users.noreply.github.com>
2018-04-18 18:05:42 +0200

    function
    convert no_valid_cells2 from script to function

--- src/no_valid_cells2.m
+++ src/no_valid_cells2.m
@@ -1,20 +1,8 @@
-%Cargamos el archivo
-img=imread('../data/Image_1_Diagram_16.png');
-%Binarizacion
-img_bw=im2bw(img);
-%Etiquetar las celulas
-img_label=bwlabel(img_bw);
+function no_val=no_valid_cells2(img_label)
     x1 = img_label(1,:);
     x2= img_label(end,:);
     y1= img_label(:,1);
     y2= img_label(:,end);
 no_val=unique([x1, x2, y1', y2']);
-
- 
-
-
-        
-
-% Para guardar las imagenes labelleadas
-% imwrite( uint8(img_label), 'myLabels.png' );
-% imwrite( L, rand( 256, 3 ), 'myIndexedImage.png' );
+no_val(no_val==0) = [];
+end
