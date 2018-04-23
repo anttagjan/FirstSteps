b4c369c2e938bca769f9f45ef249c3f23d1499ac
117 <a117@pascual10.ibise.es>
2018-04-23 11:36:58 +0200

    Improve function
    Summarize the function in less lines

--- src/no_valid_cells2.m
+++ src/no_valid_cells2.m
@@ -1,8 +1,4 @@
 function no_val=no_valid_cells2(img_label)
-    x1 = img_label(1,:);
-    x2= img_label(end,:);
-    y1= img_label(:,1);
-    y2= img_label(:,end);
-no_val=unique([x1, x2, y1', y2']);
+no_val=unique([img_label(1,:), img_label(end,:), img_label(:,1)', img_label(:,end)']);
 no_val(no_val==0) = [];
 end
