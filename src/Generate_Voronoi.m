02dbeca773a7f4a34de04ac735b050da4974adcc
anttagjan <atagua-ibis@us.es>
2018-06-14 18:45:23 +0200

    Change name of a script and almost finish the script Generate_Voronoi

--- src/Generate_Voronoi.m
+++ src/Generate_Voronoi.m
@@ -1,12 +1,20 @@
-img=zeros(512,512);
-x = gallery('uniformdata',[1 10],0);
-y = gallery('uniformdata',[1 10],1);
+
+%Generate a Voronoi diagram with n seeds
+n=input('Number of seeds :');
+x = gallery('uniformdata',[1 n],0);
+y = gallery('uniformdata',[1 n],1);
 voronoi(x,y,'k');
+
+%Save the voronoi diagram as an image
 img_voronoi = frame2im(getframe(gca));
-se = strel('disk',4);
-img_erode=imclose(img,ones(8,8));
-img_edge=edge(img_erode);
+img_voronoi=img_voronoi(:,:,1);
+
+%Segmentate the image to eliminate the centroid, to binarize the image and
+%establish a resolution of 512x512.
+se = strel('disk',2);
+img_edge=edge(img_voronoi);
 img_dilate=imdilate(img_edge,se);
+img_label=bwlabel(img_watershed);
 
 
 
