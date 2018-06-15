6686d1cb44ab32d1ed9dc4f9a5d91e17ce7cbde2
anttagjan <atagua-ibis@us.es>
2018-06-15 10:26:15 +0200

    Finish the script Generate_Voronoi

--- src/Generate_Voronoi.m
+++ src/Generate_Voronoi.m
@@ -1,4 +1,7 @@
-
+function Generate_Voronoi
+%Clean other stuffs
+clc 
+close all
 %Generate a Voronoi diagram with n seeds
 n=input('Number of seeds :');
 x = gallery('uniformdata',[1 n],0);
@@ -8,14 +11,18 @@ voronoi(x,y,'k');
 %Save the voronoi diagram as an image
 img_voronoi = frame2im(getframe(gca));
 img_voronoi=img_voronoi(:,:,1);
+img_cropped = imresize(img_voronoi, [512, 512]);
 
 %Segmentate the image to eliminate the centroid, to binarize the image and
 %establish a resolution of 512x512.
-se = strel('disk',2);
-img_edge=edge(img_voronoi);
+img_edge=edge(img_cropped);
+se = strel('disk',4);
 img_dilate=imdilate(img_edge,se);
+img_watershed=watershed(img_dilate);
 img_label=bwlabel(img_watershed);
+figure,imshow(img_label)
 
+end
 
 
 
