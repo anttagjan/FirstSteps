2bf9fa5375754627065ddb1664c800661035bca3
anttagjan <atagua-ibis@us.es>
2018-07-23 18:02:26 +0200

    Improving script
    Bwdist implementation in the script, but it has not finished yet.

--- src/muscle_segmentation.m
+++ src/muscle_segmentation.m
@@ -1,5 +1,12 @@
 img=imread('../data/G93A-H-100-88-SD-CMSD-1B40X.jpg');
 img_green=img(:,:,2);
-img_edge=edge(img_green);
-se=strel('disk',8);
-img_dilate=imdilate(img_edge,se);
+img_bw=im2bw(img_green);
+
+se=strel('disk',4);
+img_dilate=imdilate(img_bw,se);
+figure,imshow(img_dilate)
+img_skel=bwmorph (img_dilate,'skel',Inf);
+
+
+img_dist=bwdist(img_skel);
+img_w=watershed(img_dist);
