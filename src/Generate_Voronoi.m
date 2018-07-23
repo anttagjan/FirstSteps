b545bd695d3b819ad40c4d910e1c1644184f84be
anttagjan <atagua-ibis@us.es>
2018-07-23 16:29:00 +0200

    Changing Generate_Voronoi
    Bwdist implementation in the script. Better way to generate a Voronoi Diagram than the function voronoi

--- src/Generate_Voronoi.m
+++ src/Generate_Voronoi.m
@@ -2,27 +2,17 @@ function Generate_Voronoi
 %Clean other stuffs
 clc 
 close all
-%Generate a Voronoi diagram with n seeds
+%Generate seeds in a binary image.
+img_bw = zeros(1024,1024);
 n=input('Number of seeds :');
-x = gallery('uniformdata',[1 n],0);
-y = gallery('uniformdata',[1 n],1);
-voronoi(x,y,'k');
+seeds=unique(round(1000*rand(1,n))); 
+img_bw(seeds,seeds)=1;
 
-%Save the voronoi diagram as an image
-img_voronoi = frame2im(getframe(gca));
-img_voronoi=img_voronoi(:,:,1);
-img_cropped = imresize(img_voronoi, [512, 512]);
+%Generate the Voronoi Diagram.
+D=bwdist(img_bw);
+img_w=watershed(D);
+figure, image(img_w), colormap(jet);
 
-%Segmentate the image to eliminate the centroid, to binarize the image and
-%establish a resolution of 512x512.
-img_edge=edge(img_cropped);
-se = strel('disk',4);
-img_dilate=imdilate(img_edge,se);
-img_watershed=watershed(img_dilate);
-c=input('Choose type of connectivity: ');
-img_label=bwlabel(img_watershed,c);
-figure,imshow(img_label)
-imwrite(img_label,'../results/Voronoi_Diagram.jpg')
 end
 
 
