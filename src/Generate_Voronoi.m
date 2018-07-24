5284214697d90ee294e20e27a8d814bf924bde29
anttagjan <atagua-ibis@us.es>
2018-07-24 16:43:09 +0200

    Fix minor problems
    -Add to save the image
    
    -Improve the generation of random seeds

--- src/Generate_Voronoi.m
+++ src/Generate_Voronoi.m
@@ -5,8 +5,8 @@ close all
 %Generate seeds in a binary image.
 img_bw = zeros(512,512);
 n=input('Number of seeds :');
-x = round(512*gallery('uniformdata',[1 n],0));
-y = round(512*gallery('uniformdata',[1 n],1)); 
+x = round(512*rand(1,n));
+y = round(512*rand(1,n)); 
 M=[x ; y];
 for i=1:n 
 img_bw(M(1,i),M(2,i))=1;
@@ -20,7 +20,7 @@ se=strel('disk',2);
 img_dilate=imdilate(img_bw2,se);
 figure,imshow(img_dilate)
 
-
+imwrite(img_dilate,'../results/Voronoi_Diagram.jpg')
 end
 
 
