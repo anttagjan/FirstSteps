bfe44eae271bd5b4e26c94aa37004f328cc9b20b
anttagjan <atagua-ibis@us.es>
2018-09-12 15:03:51 +0200

    Trying to improve the script
    Probing ways to improve the script to establish the boundaries of the muscle cells

--- src/muscle_segmentation.m
+++ src/muscle_segmentation.m
@@ -7,25 +7,59 @@ level = graythresh(img_green);
 img_bw= im2bw(img_green,level);
 figure, imshow(img_bw)
 
-img_close=imclose(imopen(img_bw,ones(8,8)),ones(8,8));
-img_boundary = bwmorph(img_close,'remove');
-img_skel=bwmorph (img_boundary,'skel',Inf); 
+% Curation of the image to define the boundaries of the cells
+se=strel('disk',4);
+img_dilate=imdilate(img_bw,se);
 
-img_dist=bwdist(~img_close);
-figure,imshow(img_dist)
-img_w=watershed(img_dist);
-figure,imshow(img_w)
-
-se=strel('disk',2);
-img_erode=imerode(img_close,se);
+se=strel('disk',4);
+img_erode=imerode(img_dilate,se);
 figure,imshow(img_erode)
 
-img_skel=bwskel (img_erode);
+img_close=imclose(imopen(img_erode,ones(8,8)),ones(8,8));
 
+se=strel('square',10);
+img_dilate2=imdilate(img_close,se);
+figure,imshow(img_dilate2)
+
+img_boundaries = bwperim(img_dilate,8);
+figure,imshow(img_boundaries)
+img_dist=bwdist(img_boundaries);
+figure,imshow(img_dist)
+se=strel('square',2);
+img_dilate3=imdilate(img_boundaries,se);
+figure,imshow(img_dilate3)
+
+img_areaopen = bwareaopen(img_dilate2,1000);
+figure,imshow(img_areaopen)
 se=strel('disk',2);
-img_dilate=imdilate(img_skel,se);
-figure,imshow(img_dilate)
+img_dilate4=imdilate(img_areaopen,se);
+figure,imshow(img_dilate4)
 
-img_dist=bwdist(img_dilate);
-img_w=watershed(img_dist);
-figure,imshow(img_w)
+[centers,radii] = imfindcircles(img_dilate4,[100 160],'ObjectPolarity','dark','Sensitivity',0.95);
+
+
+
+
+
+% 
+% Find Circles
+% 
+% 
+% [centers, radii, metric] = imfindcircles(img_erode,[10000 20000]);
+% 
+% viscircles(centers, radii,'EdgeColor','b');
+% 
+% img_fillholes = imfill(~img_dilate2,'holes');
+% figure,imshow(img_fillholes)
+% 
+% img_areaopen = bwareaopen(img_erode,10);
+% figure,imshow(img_areaopen)
+
+% img_dist=bwdist(img_areaopen);
+% figure,imshow(img_dist)
+% img_w=watershed(img_dist);
+% figure,imshow(img_w)
+% 
+% imwrite(img_bw,'../results/muscle_binary.jpg')
+% imwrite(img_dist,'../results/muscle_segmentated.jpg')
+% imwrite(img_w,'../results/muscle_watershed.jpg')
